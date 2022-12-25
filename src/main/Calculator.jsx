365d4b4ad58015e430b077e306bfb041801da8ec
Rafa Malta <107279228+82rafamalta@users.noreply.github.com>
2022-12-25 16:55:21 -0300

    Add files via upload

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+export default class Caculator extends Component {
+  rendr() {
+    return <div className="calcuator"></div>;
+  }
+}
